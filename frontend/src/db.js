04e95730f35a025a62269806f47643d9056c4029
rhcndrsn <rhcn@cit.one>
2024-04-05 04:08:18 +0300

    prop, one way data flow, v-for sample

---
+++ frontend/src/db.js
@@ -0,0 +1,104 @@
+const books = [
+    {
+        id: 1,
+        name: 'The Enigma',
+        author: 'Jane Smith',
+        page: 320,
+        description:
+            'A thrilling mystery that keeps readers on the ege of their seats.',
+        rating: 8.5,
+        uploadDate: '24 Mar 2024',
+    },
+    {
+        id: 2,
+        name: 'Beyond The Stars',
+        author: 'Michael Johnson',
+        page: 412,
+        description:
+            'An epic science fiction adventure exploring distant galaxies and new civilizations.',
+        rating: 9.2,
+        uploadDate: '25 Mar 2024',
+    },
+    {
+        id: 3,
+        name: "Serendipity's Secret",
+        author: 'Emily Davis',
+        page: 280,
+        description:
+            'A heartwarming romance novel about unexpected love in a small town.',
+        rating: 7.8,
+        uploadDate: '26 Mar 2024',
+    },
+    {
+        id: 4,
+        name: 'Shadows of Destiny',
+        author: 'Jane Smith',
+        page: 320,
+        description:
+            'A fantasy epic filled with magic, prohecies, and a battle between light and darkness.',
+        rating: 8.9,
+        uploadDate: '27 Mar 2024',
+    },
+    {
+        id: 5,
+        name: 'Culinary Chronicles',
+        author: 'Maria Rodriguez',
+        page: 150,
+        description:
+            'A collection of delightful short stories centered around food and culture.',
+        rating: 7.2,
+        uploadDate: '28 Mar 2024',
+    },
+    {
+        id: 6,
+        name: 'Echoes of Eternity',
+        author: 'Benjamin Turner',
+        page: 600,
+        description:
+            'A philosophical exploration of life, time, and the universe.',
+        rating: 9.7,
+        uploadDate: '29 Mar 2024',
+    },
+    {
+        id: 7,
+        name: 'Midnight Murders',
+        author: 'Olivia Parker',
+        page: 320,
+        description:
+            "A gripping thriller following a detective's pursuit of a cunning serial killer.",
+        rating: 8.1,
+        uploadDate: '30 Mar 2024',
+    },
+    {
+        id: 8,
+        name: 'The Artistic Odyssey',
+        author: 'Christopher Bennett',
+        page: 240,
+        description:
+            "A coming-of-age story about a young artist's journey to self-discovered.",
+        rating: 7.6,
+        uploadDate: '31 Mar 2024',
+    },
+    {
+        id: 9,
+        name: 'Whispers in the Woods',
+        author: 'Samantha Foster',
+        page: 280,
+        description:
+            'A collection of eerie and spine-chilling short stories set in the woods.',
+        rating: 8.3,
+        uploadDate: '01 Apr 2024',
+    },
+    {
+        id: 10,
+        name: 'Legends of Lyria',
+        author: 'Daniel Harris',
+        page: 450,
+        description:
+            'An epic fantasy series filled with knights, dragons, and quests for honor.',
+        rating: 4.8,
+        uploadDate: '03 Apr 2024',
+    },
+];
+
+export default books;
